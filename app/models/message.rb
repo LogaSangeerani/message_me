8dc2233bb0ac3f55b0772020d9e2d3adc632916d
LogaSangeerani <logasivanantham@gmail.com>
2020-05-15 19:42:20 +0530

    Added Functionality

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true,  length: { minimum: 2, maximum: 50 }
+    scope :custom_display, ->  { order(:created_at).last(20) }
 end
